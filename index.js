e4f4c5e51b0c53557bea3211654473d904dc5cc3
riley soloner <rileysoloner@gmail.com>
2022-07-17 17:26:36 -0400

    commit tree 9d234187a4d9cf28ae74f0013b53f473389296c5

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (param) => {
+return param.slice(0, 2)
+}
+
+const returnLastTwoDrivers = (param) => {
+return param.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (multiplier) => {
+    return (fare) => {
+     return fare * multiplier
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (names, func) => {
+    return func(names)
+}
